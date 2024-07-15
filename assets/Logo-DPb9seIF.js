09b0d976eb72a6b9a5fb6f36ad326dcc513d9924
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-15 07:49:25 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs97_front_AnnaRychkova1@229a65bfc51ec914d5d4a3ff6374a30c245bc4ae 🚀

---
+++ assets/Logo-DPb9seIF.js
@@ -0,0 +1,2 @@
+import{u as s,j as t}from"./index-DrCtcoIf.js";const n="_logoFont_1kx5k_1",l={logoFont:n},r=()=>{const{t:o}=s();return t.jsx("span",{className:l.logoFont,children:o("logo.logo")})};export{r as L};
+//# sourceMappingURL=Logo-DPb9seIF.js.map
