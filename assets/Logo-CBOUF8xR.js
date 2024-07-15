83ed6f353344d0a7b99e326d7950817a2bab273c
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-15 07:48:48 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs97_front_AnnaRychkova1@3fc1b221668a9c49543d2d872c923674627c314f 🚀

--- assets/Logo-CBOUF8xR.js
+++
@@ -1,2 +0,0 @@
-import{u as s,j as t}from"./index-B6pWEu2R.js";const n="_logoFont_1kx5k_1",l={logoFont:n},r=()=>{const{t:o}=s();return t.jsx("span",{className:l.logoFont,children:o("logo.logo")})};export{r as L};
-//# sourceMappingURL=Logo-CBOUF8xR.js.map
